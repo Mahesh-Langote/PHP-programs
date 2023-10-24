40a24b2ec7c22d89388cd53c304d24366c30ea90
Mahesh Langote <maheshlangote777@gmail.com>
2023-10-24 20:30:24 +0530

    Static Methods and Properties

---
+++ StaticMethodsProperties.php
@@ -0,0 +1,17 @@
+<?php
+class Counter {
+    public static $count = 0;
+
+    public static function increment() {
+        self::$count++;
+    }
+
+    public static function getCount() {
+        return self::$count;
+    }
+}
+
+Counter::increment();
+Counter::increment();
+echo "Count: " . Counter::getCount();
+?>
