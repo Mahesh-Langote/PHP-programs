81fdd55fc6bf9cf85c294859bc674a77eba534d4
Mahesh Langote <maheshlangote777@gmail.com>
2023-10-31 20:35:54 +0530

    Interface

---
+++ Interfaces.php
@@ -0,0 +1,20 @@
+<?php
+interface Shape {
+    public function calculateArea();
+}
+
+class Circle implements Shape {
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function calculateArea() {
+        return pi() * $this->radius * $this->radius;
+    }
+}
+
+$circle = new Circle(5);
+echo "Area of Circle: " . $circle->calculateArea();
+?>
